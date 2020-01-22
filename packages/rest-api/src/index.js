7d1fdf0dad5b7580c04eb88939be61e4230d3447
Alan Marcell <alanmarcell@live.com>
2020-01-21 21:09:24 -0300

    bodyParser urlencoded extended false

--- packages/rest-api/src/index.js
+++ packages/rest-api/src/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', routes());
 
